0ad9ebdcf04a0b733b91d95184cd781d11cde242
Pierre-Louis Guillou <plguillou@gmail.com>
2021-05-12 12:28:19 +0200

    First login and signin

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,3 +1,28 @@
+import {Button, Form} from "react-bootstrap";
+
 export default function Login() {
-    return <div className={"bg-secondary"}>Login</div>
+    return (
+        <div className={"container-fluid p-5 w-50 border border-2 rounded-3"}>
+            <Form>
+                <Form.Group controlId="formBasicEmail" className={"mb-4"}>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email"/>
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword" className={"mb-4"}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password"/>
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox" className={"mb-4"}>
+                    <Form.Check type="checkbox" label="Remember me"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
 }
--- src/components/pages/Signin.js
+++ src/components/pages/Signin.js
@@ -1,3 +1,56 @@
+import {Button, Col, Form} from "react-bootstrap";
+
 export default function Signin() {
-    return <div className={"bg-secondary"}>Signin</div>
+    return (
+    <div>
+        <Form className={"container-fluid p-5 w-50 border border-2 rounded-3"}>
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail" className={"mb-4"}>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword" className={"mb-4"}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1" className={"mb-4"}>
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+            </Form.Group>
+
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridCity" className={"mb-4"}>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridState" className={"mb-4"}>
+                    <Form.Label>State</Form.Label>
+                    <Form.Control as="select" defaultValue="Choose...">
+                        <option>Choose...</option>
+                        <option>Bourre La Reine</option>
+                        <option>LALLALLALALLAA MATTHIEU...</option>
+                        <option>...</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridZip" className={"mb-4"}>
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control />
+                </Form.Group>
+            </Form.Row>
+
+            <Form.Group id="formGridCheckbox" className={"mb-4"}>
+                <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </div>
+    );
 }
