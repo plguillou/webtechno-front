1f69723758f737c8c807b9d1758f7d8afc07717e
Lwii-U <louis.develter@gmail.com>
2021-05-15 00:04:24 +0200

    Add PresentationSection to Home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import "../../Styles/HomeStyle.css";
+import downArrowIconPath from "../../Resources/Icons/down-arrow.png";
 
 export default function Home() {
     return (
         <>
             <HookSection/>
+            <PresentationSection/>
         </>
     );
 }
@@ -15,10 +17,41 @@ function HookSection(props) {
              id="hookSection">
             <div className="d-flex justify-content-center align-items-center w-75"
                  style={{backgroundColor: "rgba(196, 196, 196, 0.25)", backdropFilter: "blur(4px)", borderRadius: "10px"}}>
-                <h1 className="text-center lh-base"
+                <h2 className="text-center lh-base"
                     style={{fontSize: "4.6em", color: "#010D14", textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"}}>
                     Browse among hundreds of housings and book the one of your dreams!
+                </h2>
+            </div>
+        </div>
+    );
+}
+
+function PresentationSection(props) {
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center p-4"
+             style={{backgroundColor: "#023047"}}>
+            <div className="d-flex flex-column justify-content-center align-items-center w-75">
+                <h1 className="text-center lh-base p-3 mt-5"
+                    style={{fontSize: "5em", color: "#FFB703"}}>
+                    Home Exchange Manager
                 </h1>
+                <p className="text-center lh-base fs-1 pb-3 mb-4" style={{color: "#8ECAE6"}}>
+                    indexes <em className="fst-normal" style={{color: "#FFB703"}}>hundreds of housings</em>,
+                    owned by private individuals, that you can book for a night
+                    to a few weeks.
+                </p>
+                <p className="text-center lh-base fs-1 pt-3 mt-4" style={{color: "#8ECAE6"}}>
+                    You are looking for your dream vacation house, or just for a place to sleep during your exam week?
+                </p>
+                <a href="#searchBar" className="text-center text-decoration-none">
+                    <p className="text-center lh-base fs-1" style={{color: "#FB8500"}}>
+                        Stop bothering, start browsing!
+                    </p>
+                    <div className="p-4 my-4">
+                        <img src={downArrowIconPath} alt="Icon showing an arrow pointing downwards"
+                              style={{width: "8vw"}}/>
+                    </div>
+                </a>
             </div>
         </div>
     );
