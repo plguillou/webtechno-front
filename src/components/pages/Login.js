a1b988aa9f0114226dfd6dd24e8808bed137490d
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-14 19:26:14 +0200

    login page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,3 +1,28 @@
+import {useCallback} from "react";
+import {loginAttempt} from "../utils/store/user/userActions";
+import {useDispatch} from "react-redux";
+
 export default function Login() {
-    return <div className={"bg-secondary"}>Login</div>
+    const dispatch = useDispatch()
+    const logUser = useCallback((username, password) => {
+        dispatch(loginAttempt(username, password))
+    }, [dispatch])
+
+    let username = "";
+    let password = "";
+
+    const handleLogClick = () => {
+        logUser(username, password)
+    }
+
+    return <div>
+        <p>Login Page</p>
+        <label htmlFor={"username"}>
+            <input type="text" id={"username"} onChange={(event => username = event.target.value)}/>
+        </label>
+        <label htmlFor={"password"}>
+            <input type="text" id={"password"} onChange={(event => password = event.target.value)}/>
+        </label>
+        <button onClick={handleLogClick}>Log</button>
+    </div>
 }
