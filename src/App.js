688f671655ec2bb1ad001d4bfe14551502cde36c
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-14 20:07:44 +0200

    exception page

--- src/App.js
+++ src/App.js
@@ -4,23 +4,17 @@ import Home from "./components/pages/Home";
 import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import Login from "./components/pages/Login";
 import Signin from "./components/pages/Signin";
-import {Provider} from "react-redux";
+import {Provider, useSelector} from "react-redux";
 import store from "./components/utils/store";
 import Profile from "./components/pages/Profile";
+import {userSelector} from "./components/utils/store/user/userSelector";
+import Routes from "./components/utils/Routes";
 
 function App() {
     return (
         <>
             <Provider store={store}>
-                <Router>
-                    <Header/>
-                    <Switch>
-                        <Route path="/login" component={Login}/>
-                        <Route path="/signin" component={Signin}/>
-                        <Route path="/profile" component={Profile}/>
-                        <Route path="/" component={Home}/>
-                    </Switch>
-                </Router>
+                <Routes />
             </Provider>
         </>
     );
