cfeb2b1fe8849ea06da2d4f09d25823b507a8912
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-14 16:35:16 +0200

    composeWithDevTools + localstorage token

--- src/components/utils/store/index.js
+++ src/components/utils/store/index.js
@@ -1,4 +1,5 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import userReducer from "./user/UserReducer";
 import thunk from "redux-thunk";
 import axios from "axios";
@@ -6,12 +7,18 @@ import axios from "axios";
 
 const store = createStore(combineReducers({
     user: userReducer
-}), applyMiddleware(thunk))
+}), undefined,
+    composeWithDevTools(
+    applyMiddleware(thunk)
+    )
+)
 
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-console.log("test")
-if (localStorage.jwtToken) {
-    axios.defaults.headers.common['Authorization'] = `${localStorage.jwtToken}`;
+
+if (localStorage.authJwtToken) {
+    console.log("token found in local storage")
+    axios.defaults.headers.common['Authorization'] = `${localStorage.authJwtToken}`;
+    // store.dispatch() //get user info
 } else {
     delete axios.defaults.headers.common['Authorization'];
 }
