0fa5af8a538f7462a21fa1eaa06ca665e819e44c
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-19 16:39:42 +0200

    test custom toggle

--- src/components/pages/HouseDetails.js
+++ src/components/pages/HouseDetails.js
@@ -1,7 +1,7 @@
 import {getHouseDetails, modifyHouseDetails} from "../utils/requests/houses";
-import {useEffect, useState} from "react";
+import {useEffect, useState, forwardRef, Children} from "react";
 import {useParams} from "react-router-dom";
-import {Button} from "react-bootstrap";
+import {Button, Dropdown, FormControl} from "react-bootstrap";
 
 function HouseDetails() {
     let {id} = useParams();
@@ -41,7 +41,7 @@ function HouseDetails() {
                 <div>Titre de votre résidence :</div>
                 <div className={"m-auto"}>
                     <input className={"border bg-light text-fogra29"} value={newTitle}
-                                                 disabled={!isEditingHouse} onChange={(e) => setNewTitle(e.target.value)}/>
+                           disabled={!isEditingHouse} onChange={(e) => setNewTitle(e.target.value)}/>
                 </div>
             </div>
             <br/>
@@ -52,6 +52,13 @@ function HouseDetails() {
                            disabled={!isEditingHouse} onChange={(e) => setNewDescription(e.target.value)}/>
                 </div>
             </div>
+            <DropdownCustom items={house.constraints}/>
+            {/* todo faire une fonction utils qui
+                param : listeSelected, listNotSelected
+                out: newlisteSelected, newlisteNotSelected -> setter pour les listes en param
+                print: list selected sous form de liste de chimp, print les not selected dans un menu deroulant
+            */}
+
             <div>
                 {
                     isEditingHouse && <div>
@@ -62,6 +69,60 @@ function HouseDetails() {
             </div>
         </div>
     </>
+}
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const CustomToggle = forwardRef(({children, onClick}, ref) => (
+    <a
+        href=""
+        ref={ref}
+        onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+        }}
+    >
+        {children}
+        &#x25bc;
+    </a>
+));
+
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const CustomMenu = forwardRef(
+    ({children, style, className, 'aria-labelledby': labeledBy}, ref) => {
+        const [value, setValue] = useState('');
+
+        return (
+            <div ref={ref} style={style} className={className} aria-labelledby={labeledBy}>
+                <FormControl autoFocus className="mx-3 my-2 w-auto" placeholder="Type to filter..."
+                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                />
+                <ul className="list-unstyled">
+                    {Children.toArray(children).filter(
+                        (child) =>
+                            !value || child.props.children.toLowerCase().startsWith(value),
+                    )}
+                </ul>
+            </div>
+        );
+    },
+);
+
+function DropdownCustom({items}) {
+    return <Dropdown>
+        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+            Custom toggle
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu as={CustomMenu}>
+            {items?.map((elem, i) => (
+                <Dropdown.Item eventKey={i}>{elem.title}</Dropdown.Item>
+            ))}
+        </Dropdown.Menu>
+    </Dropdown>
 }
 
 export default HouseDetails;
