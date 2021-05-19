16ac101be87bd2ea9879e5a9525de7fc30f54db5
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-19 20:55:17 +0200

    House details constraints modification (only graphic)

--- src/components/pages/HouseDetails.js
+++ src/components/pages/HouseDetails.js
@@ -1,9 +1,19 @@
 import {getHouseDetails, modifyHouseDetails} from "../utils/requests/houses";
-import {useEffect, useState, forwardRef, Children} from "react";
+import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {Button, Dropdown, FormControl} from "react-bootstrap";
+import {Button, OverlayTrigger, Tooltip} from "react-bootstrap";
+import MultiChoiceList from "../common/MultiChoiceList";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllHouseConstraints} from "../utils/store/house-constraint/houseConstraintAction";
+import {houseConstraintSelector} from "../utils/store/house-constraint/houseConstraintSelector";
 
 function HouseDetails() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getAllHouseConstraints())
+    }, [])
+    const constraints = useSelector(houseConstraintSelector)
+
     let {id} = useParams();
     const [isEditingHouse, setIsEditingHouse] = useState(false);
     const [updateValue, setUpdateValue] = useState(false);
@@ -12,6 +22,7 @@ function HouseDetails() {
     const [house, setHouse] = useState([]);
     let [newTitle, setNewTitle] = useState("");
     let [newDescription, setNewDescription] = useState("");
+    let [newConstraints, setNewConstraints] = useState([]);
 
     useEffect(() => {
         getHouseDetails(id, setHouse)
@@ -20,7 +31,9 @@ function HouseDetails() {
     useEffect(() => {
         setNewTitle(house?.title)
         setNewDescription(house?.description)
+        setNewConstraints(house?.constraints)
     }, [house])
+
 
     const handleCancelClick = () => {
         setNewTitle(house?.title);
@@ -34,95 +47,78 @@ function HouseDetails() {
 
 
     return <>
-        <h1>Details de la house {id}</h1>
-        <Button onClick={() => setIsEditingHouse(!isEditingHouse)}>Click here to edit toggle</Button>
-        <div className={"container"}>
+        <h1 className={"text-center mt-3"}>Details de la house {id}</h1>
+        <div>{JSON.stringify(house)}</div>
+
+        <div className={"container border rounded-2 p-2"}>
+            <Button className={"float-end"} variant={!isEditingHouse ? "outline-primary" : "outline-danger"}
+                    onClick={() => setIsEditingHouse(!isEditingHouse)}>{isEditingHouse ? "X" : "Edit"}</Button>
             <div className={"container-fluid"}>
                 <div>Titre de votre résidence :</div>
                 <div className={"m-auto"}>
-                    <input className={"border bg-light text-fogra29"} value={newTitle}
-                           disabled={!isEditingHouse} onChange={(e) => setNewTitle(e.target.value)}/>
+                    <input
+                        className={"bg-light text-fogra29 border  " + (isEditingHouse ? "border-dark" : "")}
+                        value={newTitle}
+                        disabled={!isEditingHouse} onChange={(e) => setNewTitle(e.target.value)}/>
                 </div>
             </div>
             <br/>
             <div className={"container-fluid"}>
                 <div>Description de votre résidence :</div>
                 <div className={"m-auto"}>
-                    <input className={"border bg-light text-fogra29"} value={newDescription}
+                    <input className={"border bg-light text-fogra29 " + (isEditingHouse ? "border-dark" : "")}
+                           value={newDescription}
                            disabled={!isEditingHouse} onChange={(e) => setNewDescription(e.target.value)}/>
                 </div>
             </div>
-            <DropdownCustom items={house.constraints}/>
-            {/* todo faire une fonction utils qui
-                param : listeSelected, listNotSelected
-                out: newlisteSelected, newlisteNotSelected -> setter pour les listes en param
-                print: list selected sous form de liste de chimp, print les not selected dans un menu deroulant
-            */}
+            <hr/>
+            <div className={"container-fluid"}>
+                <div>Liste des contraintes liées à votre résidence :</div>
+                <div className={"container d-block"}>
+                    {newConstraints?.map((elem, i) => (
+                        <div key={i}>
 
-            <div>
-                {
-                    isEditingHouse && <div>
-                        <Button variant={"outline-success"} onClick={handleOkClick}>OK</Button>
-                        <Button variant={"outline-secondary"} onClick={handleCancelClick}>Cancel</Button>
-                    </div>
-                }
+                                <div className={"btn border border-1 text-dark bg-gray rounded-10 rounded-pill"}
+                                        key={i}
+                                >
+                                    {elem.title}
+                                    <Button variant={"outline-dark"} size={"sm"}
+                                            className={"rounded-circle border-dark border ms-2 px-1 py-0"}>
+                                        <OverlayTrigger
+                                            key={"click-to-remove"}
+                                            placement={"right"}
+                                            overlay={
+                                                <Tooltip id={"tooltip-click-to-remove"}>
+                                                    Click to remove
+                                                </Tooltip>
+                                            }>
+                                        <i className="bi bi-x"/></OverlayTrigger>
+                                    </Button>
+                                </div>
+
+
+                        </div>
+                    ))}
+                </div>
+                {isEditingHouse && <MultiChoiceList listSelected={newConstraints} listTotal={constraints}
+                                                          onSelect={(item) => setNewConstraints([item, ...newConstraints])}/>}
             </div>
+
+
+            {
+                isEditingHouse && <>
+                    <hr/>
+                    <div className={"d-flex justify-content-end"}>
+                        <Button variant={"outline-success"} onClick={handleOkClick}>OK</Button>
+                        <Button variant={"outline-secondary"}
+                                onClick={handleCancelClick}>Cancel</Button>
+                    </div>
+                </>
+            }
+
         </div>
     </>
 }
 
-// The forwardRef is important!!
-// Dropdown needs access to the DOM node in order to position the Menu
-const CustomToggle = forwardRef(({children, onClick}, ref) => (
-    <a
-        href=""
-        ref={ref}
-        onClick={(e) => {
-            e.preventDefault();
-            onClick(e);
-        }}
-    >
-        {children}
-        &#x25bc;
-    </a>
-));
-
-
-// forwardRef again here!
-// Dropdown needs access to the DOM of the Menu to measure it
-const CustomMenu = forwardRef(
-    ({children, style, className, 'aria-labelledby': labeledBy}, ref) => {
-        const [value, setValue] = useState('');
-
-        return (
-            <div ref={ref} style={style} className={className} aria-labelledby={labeledBy}>
-                <FormControl autoFocus className="mx-3 my-2 w-auto" placeholder="Type to filter..."
-                    onChange={(e) => setValue(e.target.value)}
-                    value={value}
-                />
-                <ul className="list-unstyled">
-                    {Children.toArray(children).filter(
-                        (child) =>
-                            !value || child.props.children.toLowerCase().startsWith(value),
-                    )}
-                </ul>
-            </div>
-        );
-    },
-);
-
-function DropdownCustom({items}) {
-    return <Dropdown>
-        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-            Custom toggle
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu as={CustomMenu}>
-            {items?.map((elem, i) => (
-                <Dropdown.Item eventKey={i}>{elem.title}</Dropdown.Item>
-            ))}
-        </Dropdown.Menu>
-    </Dropdown>
-}
 
 export default HouseDetails;
