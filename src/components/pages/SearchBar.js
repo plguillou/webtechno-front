515afabe52725b341b8ece753bc94c8a00d03a32
Lwii-U <louis.develter@gmail.com>
2021-05-19 23:55:59 +0200

    Add a type to inputs of search bar

--- src/components/pages/SearchBar.js
+++ src/components/pages/SearchBar.js
@@ -3,12 +3,13 @@ import {useState} from "react";
 
 export default function SearchBar() {
     return (
-        <form className="d-flex justify-content-center align-items-center rounded-pill bg-white p-2">
-            <BrowserField name="Location" placeholder="Where do you go?"/>
+        <form method="post" action=""
+              className="d-flex justify-content-center align-items-center rounded-pill bg-white p-2">
+            <BrowserField name="Location" type="text" placeholder="Where do you go?"/>
             <div className="border" style={{height: "30px"}}/>
-            <BrowserField name="Arrival" placeholder="When do you come?"/>
+            <BrowserField name="Arrival" type="date" placeholder="When do you come?"/>
             <div className="border" style={{height: "30px"}}/>
-            <BrowserField name="Departure" placeholder="When do you leave?"/>
+            <BrowserField name="Departure" type="date" placeholder="When do you leave?"/>
             <input type="submit" value=" "
                    className="border-0 rounded-circle p-0 bg-pos-center"
                    style={{backgroundImage: "url("+magnifierIcon+")", width: "65px", height: "65px"}}/>
@@ -26,7 +27,7 @@ function BrowserField(props) {
              onMouseLeave={() => setIsHovered(false)}
         >
             <h4 className="m-0 fs-5">{props.name}</h4>
-            <input type="text" name={props.name} placeholder={props.placeholder}
+            <input type={props.type} name={props.name} placeholder={props.placeholder} required
                    className="border-0 fs-6 bg-transparent"/>
         </div>
     );
