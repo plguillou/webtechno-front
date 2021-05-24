3d4d4079c4b393fb6a70f66340bf12f95f1bd1a9
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-25 00:07:02 +0200

    small changes

--- src/components/utils/store/house-constraint/houseConstraintReducer.js
+++ src/components/utils/store/house-constraint/houseConstraintReducer.js
@@ -1,6 +1,6 @@
 export const SET_HOUSE_CONSTRAINTS_ACTION = "SET_HOUSE_CONSTRAINTS_ACTION";
 
-const defaultHouseConstraints = [{test:"test"}];
+const defaultHouseConstraints = [];
 
 export default function houseConstraintReducer (state = defaultHouseConstraints, action) {
     switch (action.type) {
