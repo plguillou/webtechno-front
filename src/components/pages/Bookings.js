916b6b2ee96e6e44e8289e0648ffabb9f3a125bf
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-05-26 01:13:11 +0200

    bookings printing correctly

--- src/components/pages/Bookings.js
+++ src/components/pages/Bookings.js
@@ -3,52 +3,66 @@ import {Spinner} from "react-bootstrap";
 import {getBookings, getReceivedBookings} from "../utils/requests/bookings";
 
 export default function Bookings() {
-    const [isLoading, setIsLoading] = useState(true);
+    const [is1Loading, setIs1Loading] = useState(true);
+    const [is2Loading, setIs2Loading] = useState(true);
     const [receivedBookings, setReceivedBookings] = useState([]);
     const [sentBookings, setSentBookings] = useState([]);
     useEffect(() => {
         getBookings().then((data) => {
-            console.log("sent", data)
             setSentBookings(data);
+            setIs1Loading(false)
         });
         getReceivedBookings().then((data) => {
-            console.log("received", data)
             setReceivedBookings(data);
+            setIs2Loading(false)
         });
     }, [])
 
 
+
     return <div className={"container mt-4"}>
-        <h2 className={"text-center text-uppercase"}>Vos réservations</h2>
+        <h2 className={"text-center text-uppercase m-4"}>Vos réservations</h2>
         <div className={"container border border-gray rounded-2 my-2"}>
             <h2 className={"m-2"}>Les réservations que vous avez envoyés</h2>
             {
-                isLoading &&
+                is1Loading &&
                 <div className={"d-flex justify-content-center m-4"}>
                     <Spinner animation={"border"} variant={"honey"}/>
                 </div>
             }
-            <div>
-                {sentBookings.map((elem, i) => <div key={i} className={"d-inline-flex border border-light m-2"}>
-                    <div className={"m-1"}>Demande de &lt;todo&gt;</div>
-                    <div className={"m-1"}>Pour la maison &lt;todo&gt;</div>
-                    <div className={"m-1"}>Du {elem.startDate} au {elem.endDate}</div>
-                </div>)}
+            <div className={"d-flex align-content-center flex-column"}>
+                {
+                    sentBookings.map((elem, i) => <div key={i} className={"d-inline-flex border border-honey m-auto my-2 p-2"}>
+                            <div className={"m-1"}>Demande de votre part</div>
+                            <div className={"m-1"}>pour la résidence "{elem.house.title}"</div>
+                            <div className={"m-1"}>du <b>{dateStringToLabel(elem.startDate)}</b> au <b>{dateStringToLabel(elem.endDate)}</b></div>
+                        </div>)
+                }
             </div>
         </div>
         <div className={"container border border-gray rounded-2 my-2"}>
             <h2 className={"m-2"}>Les réservations que l'on vous a envoyés</h2>
             {
-                isLoading &&
+                is2Loading &&
                 <div className={"d-flex justify-content-center m-4"}>
                     <Spinner animation={"border"} variant={"honey"}/>
                 </div>
             }
-            <div>
-                {JSON.stringify(receivedBookings)}
+            <div className={"d-flex align-content-center flex-column"}>
+                {
+                    receivedBookings.map((elem, i) => <div key={i} className={"d-inline-flex border border-orange m-auto my-2 p-2"}>
+                        <div className={"m-1"}>Demande de <b>{elem.user.name}</b></div>
+                        <div className={"m-1"}>pour la résidence "{elem.house.title}"</div>
+                        <div className={"m-1"}>du <b>{dateStringToLabel(elem.startDate)}</b> au <b>{dateStringToLabel(elem.endDate)}</b></div>
+                    </div>)
+                }
             </div>
         </div>
 
-
     </div>
+}
+
+const dateStringToLabel = (dateString) => {
+    const date = new Date(dateString);
+    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
 }
