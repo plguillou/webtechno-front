5d94821a1cbfbbe3b94552676d4d618224cf8e54
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-06-04 00:20:04 +0200

    re get user infos after adding house

--- src/components/pages/HousesList.js
+++ src/components/pages/HousesList.js
@@ -3,8 +3,11 @@ import {useEffect, useState} from "react";
 import {Button, Table} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import AddHouseModal from "../common/house/AddHouseModal";
+import {getUserInfos} from "../utils/store/user/userActions";
+import {useDispatch} from "react-redux";
 
 function HousesList() {
+    const  dispatch = useDispatch();
     const [isAddingHouse, setIsAddingHouse] = useState(false);
     const [updateValue, setUpdateValue] = useState(false);
     const update = () => setUpdateValue(updateValue+1);
@@ -19,11 +22,12 @@ function HousesList() {
         console.log(newHouseTitle, newHouseDescription)
         addHouse(newHouseTitle, newHouseDescription, update)
         setIsAddingHouse(false);
+        setTimeout(() => dispatch(getUserInfos()), 800)
     }
 
     return <>
-    <body  class="mb-2 mt-2">
-        <h1 class="mb-3">Liste de vos houses</h1>
+    <body  className="mb-2 mt-2">
+        <h1 className="mb-3">Liste de vos houses</h1>
         <Table striped bordered hover>
             <thead>
                 <tr>
@@ -37,21 +41,21 @@ function HousesList() {
             </thead>
 
             <tbody>
-               
+
                 {
                     houses.map((elem, i) => (
                         <tr>
-                       
 
-                   
+
+
                             <td>{i + 1}</td>
                             <td>{elem.title ? elem.title.replace(/(.{25})..+/, "$1 ...") : null}</td>
                             <td>{elem.description ? elem.description.replace(/(.{90})..+/, "$1 ...") : null}</td>
                             <td>{elem.city ? elem.city.replace(/(.{90})..+/, "$1 ...") : null}</td>
                             <td><Link to={"/house-details/" + elem.id}><Button variant={"outline-secondary"}>Voir les détails</Button></Link></td>
-                          
-                            
-                       
+
+
+
                          </tr>
                     ))
                 }
