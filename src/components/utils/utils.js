92e2ab1529dfad04978e246bd5268b6158881dcd
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-06-04 23:34:40 +0200

    new utils function to format dates

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,5 +1,18 @@
-export const dateStringToLabel = (dateString) => {
-    if(!dateString) return "";
+export const dateStringToLabel = (dateString, separator = "/") => {
+    if (!dateString) return "";
     const date = new Date(dateString);
-    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    return date.getDate() + separator + date.getMonth() + separator + date.getFullYear();
+}
+
+
+export const toYYYYMMDD = (dateString, separator = "") => {
+    const date = new Date(dateString);
+    const mm = date.getMonth() + 1; // getMonth() is zero-based
+    const dd = date.getDate();
+
+    return [date.getFullYear(),
+        (mm > 9 ? '' : '0') + mm,
+        (dd > 9 ? '' : '0') + dd
+    ].join(separator);
+
 }
