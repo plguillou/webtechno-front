4242fd23b06f1350508a780facf86a07a953095d
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-06-06 01:20:56 +0200

    fix date error (months) but still a shift in the days

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,16 +1,17 @@
 export const dateStringToLabel = (dateString, separator = "/") => {
     if (!dateString) return "";
     const date = new Date(dateString);
-    return date.getDate() + separator + date.getMonth() + separator + date.getFullYear();
+    return date.getDate() + separator + (date.getMonth() + 1) + separator + date.getFullYear();
 }
 
 
 export const toYYYYMMDD = (dateString, separator = "") => {
     const date = new Date(dateString);
-    const mm = date.getMonth() + 1; // getMonth() is zero-based
+    const mm = date.getMonth() + 1 ; // getMonth() is zero-based
     const dd = date.getDate();
+    const yyyy = "0".repeat(Math.max(0, 4-date.getFullYear().toString().length));
 
-    return [date.getFullYear(),
+    return [yyyy + date.getFullYear(),
         (mm > 9 ? '' : '0') + mm,
         (dd > 9 ? '' : '0') + dd
     ].join(separator);
