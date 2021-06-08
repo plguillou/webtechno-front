5f391b6667519e5ea081b450817609ffb8c70e1d
Pierre-Louis Guillou <plguillou@gmail.com>
2021-06-08 12:53:05 +0200

    first version basic view
    msg + contacts + send msg

--- src/components/pages/Conversations.js
+++ src/components/pages/Conversations.js
@@ -54,144 +54,128 @@ function Conversations() {
 
     return <>
 
-        <div className={"d-lg-inline-flex justify-content-center container w-100 pt-2 m-2"}>
-            <div className={"w-25"}>
-                <div className={"container-fluid"}>
-                    <div>
-                        <Form.Control
-                            type={"text"}
-                            className={"bg-light text-fogra29 border  " + (isEditingProfile ? "border-dark" : "")}
-                            value={otherUser?.name}
-                            disabled={!isEditingProfile}
-                            onChange={(e) => setOtherUser({...otherUser, name: e.target.value})}/>
+        <div style={{height: '85vh', width: '99vw'}} className={"d-lg-inline-flex justify-content-evenly"}> {/* Contient tout */}
+
+            <div style={{width: '25vw'}} className={"border-end"}>{/* La partie contact */}
+                <div>
+                    <div className={"p-3 border-bottom"}>
+                        Contact 1
+                    </div>
+                    <div className={"p-3 border-bottom"}>
+                        Contact 2
+                    </div>
+                    <div className={"p-3 border-bottom"}>
+                        Contact 3
+                    </div>
+                    <div className={"p-3 border-bottom"}>
+                        Contact 4
                     </div>
                 </div>
             </div>
-            <div className={""}>
-                {isEditingProfile ?
-                    ""
-                    :
-                    <Button className={"float-end"}
-                            variant={"outline-primary"}
-                            onClick={() => setIsEditingProfile(true)}>
-                        Edit
-                    </Button>
-                }
-            </div>
-            <div>
-                {
 
-                    isEditingProfile && <>
-                        <div className={"d-flex justify-content-end"}>
-                            <div className={"mx-2"}>
-                                <Button variant={"outline-success"} onClick={handleOkClick}>OK</Button>
-                            </div>
-                            <div className={"mx-2"}>
-                                <Button variant={"outline-secondary"} onClick={handleCancelClick}>Cancel</Button>
+            <div style={{width: '75vw'}} className={""}>{/* La partie messages */}
+                <div style={{height: '67vh', display: 'block', overflowY: "auto"}} className={"container"}>
+                    {/*<div className={"w-75 mx-5 pt-4 pb-5 pt-lg-0"}>*/}
+
+                    <Message content={newUser?.name} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+                    <Message content={newUser?.name} myMessage={true}/>
+
+                    <Message content={newUser?.mail} myMessage={false}/>
+
+
+                    <br/>
+                </div>
+
+
+
+
+                <hr/>
+
+
+
+
+                <div className={"d-lg-inline-flex justify-content-center container pt-2"}>
+                    <div className={"w-25"}>
+                        <div className={"container-fluid"}>
+                            <div>
+                                <Form.Control
+                                    type={"text"}
+                                    className={"bg-light text-fogra29 border  " + (isEditingProfile ? "border-dark" : "")}
+                                    placeholder={"Enter new message here ..."}
+                                    onChange={(e) => setOtherUser({...otherUser, name: e.target.value})}/>
                             </div>
                         </div>
-                    </>
-                }
-            </div>
-        </div>
-
-        <hr/>
-
-        <div className={"d-lg-inline-flex justify-content-evenly container w-100"}>
-            <div className={"w-75 mx-5 pt-4 pb-5 pt-lg-0 "}>
-
-                <Message title={"Name"} variable={newUser?.name} isEditingProfile={isEditingProfile}
-                         myMessage={false}/>
-
-                <br/>
-                <Message title={"Name"} variable={newUser?.name} isEditingProfile={isEditingProfile}
-                         myMessage={true}/>
-
-                <br/>
-                <Message title={"E-Mail"} variable={newUser?.mail} isEditingProfile={isEditingProfile}
-                         myMessage={false}/>
-
-                <br/>
-            </div>
-        </div>
-
-
-
-
-
-
-        <hr/>
-
-        <div className={"d-lg-inline-flex justify-content-center container w-100 pt-2 m-2"}>
-            <div className={"w-25"}>
-                <div className={"container-fluid"}>
+                    </div>
+                    <div className={""}>
+                        {isEditingProfile ?
+                            ""
+                            :
+                            <Button className={"float-end"}
+                                    variant={"outline-primary"}
+                                    onClick={() => setIsEditingProfile(true)}>
+                                Send
+                            </Button>
+                        }
+                    </div>
                     <div>
-                        <Form.Control
-                            type={"text"}
-                            className={"bg-light text-fogra29 border  " + (isEditingProfile ? "border-dark" : "")}
-                            placeholder={"Enter new message here ..."}
-                            onChange={(e) => setOtherUser({...otherUser, name: e.target.value})}/>
+                        {
+
+                            isEditingProfile && <>
+                                <div className={"d-flex justify-content-end"}>
+                                    <div className={"mx-2"}>
+                                        <Button variant={"outline-success"} onClick={handleOkClick}>OK</Button>
+                                    </div>
+                                    <div className={"mx-2"}>
+                                        <Button variant={"outline-secondary"} onClick={handleCancelClick}>Cancel</Button>
+                                    </div>
+                                </div>
+                            </>
+                        }
                     </div>
                 </div>
-            </div>
-            <div className={""}>
-                {isEditingProfile ?
-                    ""
-                    :
-                    <Button className={"float-end"}
-                            variant={"outline-primary"}
-                            onClick={() => setIsEditingProfile(true)}>
-                        Edit
-                    </Button>
-                }
-            </div>
-            <div>
-                {
 
-                    isEditingProfile && <>
-                        <div className={"d-flex justify-content-end"}>
-                            <div className={"mx-2"}>
-                                <Button variant={"outline-success"} onClick={handleOkClick}>OK</Button>
-                            </div>
-                            <div className={"mx-2"}>
-                                <Button variant={"outline-secondary"} onClick={handleCancelClick}>Cancel</Button>
-                            </div>
-                        </div>
-                    </>
-                }
+
+
+
             </div>
         </div>
-
-
-
-
-
-
-
-
-        {
-
-            /*<div>
-            <input disabled value={user.name}/>
-        </div>
-        <div>
-            <input disabled value={user.mail}/>
-        </div>
-        <div>
-            <input disabled value={user.role}/>
-        </div>
-        <div>
-            <input disabled value={user.isLogged}/>
-        </div>*/}
-        <hr/>
-
-        <Link to={"/"} className={"m-5"}>
-            <Button onClick={handleLogOut} className={"px-5"}>Log out</Button>
-        </Link>
-
-        <Link to={"/houses-list"} className={"m-5"}>
-            <Button variant={"outline-secondary"} className={"px-3"}>Accedez a vos houses</Button>
-        </Link>
 
     </>
 }
@@ -210,14 +194,14 @@ const Input = ({title, variable, isEditingProfile, onInputChange, type = "text"}
     </div>
 }
 
-const Message = ({title, variable, isEditingProfile, myMessage, type = "text"}) => {
-    return <div className={"container-fluid px-2 py-3 my-1 w-100 "}>
-        <div>
-            <Form.Control
-                type={type}
-                className={"bg-light text-fogra29 w-50 border  " + (isEditingProfile ? "border-dark " : " ")  + (myMessage ? "float-end mr-1" : "float-start ml-1")}
-                value={variable ? variable : ""}
-                disabled={!isEditingProfile}/>
+const Message = ({content, myMessage}) => {
+    return <div className={"container-fluid px-2 py-3 my-1 "}>
+        <div style={{display: "block"}} className={"py-3"}>
+            <div
+                style={{borderRadius: 25}}
+                className={"bg-light text-fogra29 p-2 border " + (myMessage ? "float-end mr-1" : "float-start ml-1")}>
+                {content ? content : ""}
+            </div>
         </div>
     </div>
 }
